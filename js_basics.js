2893a7e0cfc7d73a2ffae0879ffbc2a47fc02f75
Mustafizur Rahman <mustafizur232@gmail.com>
2023-12-23 01:04:07 +0600

    first commit

---
+++ js_basics.js
@@ -0,0 +1,34 @@
+// const x = 'montu   \
+// tumi ki koro';
+// console.log(x);
+// const y = new String('montu')
+// console.log(typeof x);
+// console.log(typeof y);
+// console.log(x === y);
+// const text = 'bangladesh bangladesh is a rich country!!';
+// console.log(text.lastIndexOf('bangladesh', 14));
+
+// let x = "Templates Literals";
+// let item = ["template literals", "javascript", "es6"];
+
+// let product = `<h2>${x}</h2><ul>`;
+// for (const x of item) {
+//     product += `<li>${x}</li>`;
+// }
+
+// product += `</ul>`;
+
+// const x = 10;
+// const y = 20;
+// const z = ('the sum is') + (x + y);
+// // console.log(z);
+
+// const a = 's10';
+// const b = 100;
+// console.log(isNaN(a));
+// console.log(typeof (a));
+// console.log(isNaN(b));
+
+let n1 = 123;
+let n2 = new Number(123);
+console.log(n1 == n2);
